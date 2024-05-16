8e3cd4a6a452da313c340246249baf085f9fe909
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-05-17 01:36:20 +0500

    end

---
+++ js/index.js
@@ -0,0 +1,115 @@
+// integer 1
+
+// alert ('uzunlikning toliq metrlar sonini topish')
+// let sm = +prompt('sm')
+// let m =  Math.floor(sm/100);
+// console.log(m);
+
+// integer 2
+
+// alert ('toliq tonnalar sonini aniqlash')
+// let kg = +prompt ('kg');
+// let t = Math.floor (kg/1000);
+// console.log(t);
+
+// integer 3
+
+// alert ('fayl xajmini kb da ifodalovchi programma tuzish')
+// let b = +prompt ('b')
+// let kb = Math.floor (b/1024);
+// console.log (kb);
+
+// integer 4
+
+// alert ('a kesmada b sonini nechi marta toliq joylashtirish mumkun')
+// let a = +prompt ('a');
+// let b = +prompt ('b');
+// let result = Math.floor(a/b);
+// console.log(result);
+
+// integer 5 xato chiqdi
+
+// alert ('a kesmada b kesmaning joylashmaganan qismini toping')
+// let a = +prompt ('a');
+// let b = +prompt ('b');
+// let c = Math.floor(a/b);
+// let result = Math.floor(a+b-c);
+// console.log (c);
+// console.log (result); 
+
+// integer 6
+
+// alert ('onliklar va birliklar xonasidagi sonni topish')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor ((n/10)%10);
+// console.log (b);
+// console.log (o);
+
+// integer 7
+
+// alert ('raqamlar yegindisini topish')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor ((n/10)%10);
+// let result = Math.floor (b+o);
+// console.log (result);
+
+// integer 8
+
+// alert ('2 xonali sonni raqamlarini ornini alishtirish orqali hosil bolgan sonlarni aniqlash')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor (n/10);
+// let result = Math.floor (b*10+o);
+// console.log (result);
+
+// integer 9
+
+// alert ('3 xonali sonni yuzlar xonasidagi raqamini aniqlash')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor (n/10);
+// let y = Math.floor ((n/100)%10);
+// console.log (y);
+
+// integer 10
+
+// alert ('3 xonali son berilgan uni birliklar va onliklar xonasidagi raqamni aniqlah')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor ((n/10)%10);
+// let y = Math.floor ((n/100)%10);
+// console.log (b);
+// console.log (y);
+
+// integer 11
+
+// alert ('3 xonali sonning raqamlar yegidisini topish')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor ((n/10)%10);
+// let y = Math.floor ((n/100)%10);
+// let result = Math.floor (b+o+y);
+// console.log (result);
+
+// integer 12
+
+// alert ('3 xonali son berilgan uning raqamlari teskari tartibda yozishdan hosil bolgan sonni aniqlash')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor ((n/10)%10);
+// let y = Math.floor ((n/100)%10);
+// let result = Math.floor (b*100+o*10+y);
+// console.log (result);
+
+// integer 13
+
+// alert ('3 xonali son berilgan uning chapdan birinchi raqamini ochirib ong tarafga yozishdan hosil bolgan sonni aniqlash')
+// let n = +prompt ('n');
+// let b = Math.floor (n%10);
+// let o = Math.floor ((n/10)%10);
+// let y = Math.floor ((n/100)%10);
+// let result = Math.floor (y*100+b*10+y);
+// console.log (result);
+
